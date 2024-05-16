1fad6f0f9ff7de41b77cdee0f7280394d028667b
Gabriel Dias <125761843+gabrieldias-dev@users.noreply.github.com>
2024-05-16 16:47:37 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'uber': "url(/src/assets/bguber.png)",
+        'city': "url(/src/assets/bg-city.png)",
+      }
+    },
   },
   plugins: [],
 };
